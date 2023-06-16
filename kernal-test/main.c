5366dba2f675be009298e573b9a089a58a8a7dcc
Robert Eaglestone <robert.eaglestone@gmail.com>
2023-06-16 08:29:21 -0500

    fix: updated to r42

--- kernal-test/main.c
+++ kernal-test/main.c
@@ -5,9 +5,10 @@
 void main()
 {
    cbm_k_bsout(0x8E); // revert to primary case
+
    cbm_k_setnam("petfont.bin");
    cbm_k_setlfs(0,8,0);
-   cbm_k_load(2, 0x0f800);
+   cbm_k_load(3, 0x0f000);
 
    testAll();
 }
